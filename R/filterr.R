c136af05df788f533572b8f5c8bacb77416811b8
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-10-24 17:35:04 -0400

    Initial check-in of package documentation

---
+++ R/filterr.R
@@ -0,0 +1,10 @@
+#' filterr: A package for filtering data programmatically
+#'
+#' The filterr package provides objects representing logical
+#' conditions on rows of data.
+#' The objects allow a user to apply the same filtering query
+#' to different data sets.
+#'
+#' @docType package
+#' @name filterr
+NULL
