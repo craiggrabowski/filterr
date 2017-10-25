6ac32d3b76535cd841ff2dbf32ecf89461e969f3
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-10-24 22:54:59 -0400

    Initial check-in of true.R

---
+++ R/true.R
@@ -0,0 +1,14 @@
+#' @include object.R
+NULL
+
+TrueFilterObject <-
+setClass("TrueFilterObject", contains = "FilterObject")
+
+#' A filter object representing a tautology
+#'
+#' \code{true} returns a filter object representing a tautology.
+#'
+#' @return A filter object which is \code{TRUE} for all rows
+#'
+#' @export
+true <- function() TrueFilterObject()
