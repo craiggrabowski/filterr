821d51d25095c56a083a4a3fb23bf2bd5620af83
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-10-25 22:13:39 -0400

    Initial check-in of column source file

---
+++ R/column.R
@@ -0,0 +1,38 @@
+#' @include filterr.R
+NULL
+
+#' A class to represent a field name
+#'
+#' @exportClass Column
+setClass("Column")
+
+NamedColumn <-
+setClass("NamedColumn",
+  slots = list(
+    name = "character"
+  ),
+  contains = "Column"
+)
+
+
+#' A function returning a column name
+#'
+#' \code{column} returns a named column.
+#'
+#' @param x Name
+#'
+#' @export
+column <- function(x) NamedColumn(name = as.character(x))
+
+#' A function indicating if an object is a column
+#'
+#' \code{is_column} indicates if an object is a column.
+#'
+#' @param x Object
+#'
+#' @return \code{TRUE} / \code{FALSE} iff \code{x} is a column
+#'
+#' @export
+is_column <- function(x) is(x, "Column")
+
+
