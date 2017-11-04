4ad85f19c991577cfc1d3468c1023839cad16459
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-11-04 00:17:27 -0400

    I added a function called orEq.

--- src/logical.cpp
+++ src/logical.cpp
@@ -16,12 +16,31 @@ LT scalar_and(LT x, LT y) {
     return (x && y);
 }
 
+LT scalar_or(LT x, LT y) {
+  if (x == true)
+    return x;
+  else if (y == true)
+    return y;
+  else if (LogicalVector::is_na(x) || LogicalVector::is_na(y))
+    return NA_LOGICAL;
+  else
+    return (x || y);
+}
+
 // [[Rcpp::export]]
 LogicalVector andEq(LogicalVector x, LogicalVector y) {
   std::transform(x.begin(), x.end(),
     y.begin(), x.begin(), scalar_and);
   return x;
 }
+
+// [[Rcpp::export]]
+LogicalVector orEq(LogicalVector x, LogicalVector y) {
+  std::transform(x.begin(), x.end(),
+                 y.begin(), x.begin(), scalar_or);
+  return x;
+}
+
 
 
 
